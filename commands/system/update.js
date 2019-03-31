9b23a48ab9d58edddb86af9d33f74ea9945e570f
Dan <headhertz88@gmail.com>
2019-03-31 19:57:26 +0100

    update cmd

---
+++ commands/system/update.js
@@ -0,0 +1,31 @@
+import Command from '../../structures/Command';
+
+module.exports = class Reboot extends Command {
+  constructor( ...args ) {
+    super( ...args, {
+      name: 'update',
+      description: 'Pushes update message to all guilds into the feedback channel.',
+      category: 'System',
+      permLevel: 'Bot Owner'
+    } );
+  }
+
+  run( message, args ) {
+    if ( args.length < 1 ) {
+      return message.reply( 'Please provide a message.' );
+    }
+
+    this.client.settingsCache.forEach( async( guild ) => {
+      try {
+        const channelid = await message.verifyChannel( guild.feedbackchannel ),
+          channel = await message.guild.channels.get( channelid );
+
+        setTimeout( () => {
+          channel.send( args.join( ' ' ) );
+        }, 100 * args.join( ' ' ).length / 2 );
+      } catch ( err ) {
+        return this.client.log.debug( `Failed to send message to: ${guild.name}` );
+      }
+    } );
+  }
+};
