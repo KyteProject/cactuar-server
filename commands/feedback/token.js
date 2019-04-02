4c430f9b8400d19b86fa9617597f062b532d5946
Dan <headhertz88@gmail.com>
2019-04-02 18:02:13 +0100

    added check to token command

--- commands/feedback/token.js
+++ commands/feedback/token.js
@@ -12,21 +12,24 @@ module.exports = class Token extends Command {
     } );
   }
 
-  async run( message, [ action, ...user ] ) {
+  async run( message, [ action, ...value ] ) {
     if ( !message.settings.tokens ) {
       return message.channel.send( 'Token system has been disabled by the guild owner.' );
     }
 
-    if ( !action || !user.length ) {
+    if ( !action || !value.length ) {
       return message.channel.send( 'You must supply an action and a user.\ne.g `.token add @Dan`' );
     }
 
-    if ( action === 'add' || action === 'remove' ) {
+    if ( action === 'add' || action === 'remove' || action === 'check' ) {
       try {
         const target = message.mentions.members.first(),
-          jID = `${message.guild.id}-${target.id}`;
+          jID = `${message.guild.id}-${target.id}`,
+          user = await this.client.feedback.verifyUser( jID );
 
-        await this.client.feedback.verifyUser( jID );
+        if ( action === 'check' ) {
+          return message.channel.send( `${user.name} has: ${user.tokens} tokens.` );
+        }
 
         if ( action === 'add' ? await this.client.db.addToken( jID ) : await this.client.db.removeToken( jID ) ) {
           return message.react( '✅' );
