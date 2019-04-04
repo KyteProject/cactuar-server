7fadc8460ab500bc6ff53bc9a7af331b9ff0d100
Dan <headhertz88@gmail.com>
2019-04-04 14:34:01 +0100

    handle servers added while bot is offline

--- events/botReady.js
+++ events/botReady.js
@@ -3,17 +3,28 @@ import ActivityUpdate from './../structures/ActivityUpdate';
 
 module.exports = class extends Event {
   async run() {
-    if ( this.client.users.has( '1' ) ) {
-      this.client.users.delete( '1' );
+    try {
+      if ( this.client.users.has( '1' ) ) {
+        this.client.users.delete( '1' );
+      }
+
+      this.client.guilds.each( async( g ) => {
+        if ( !await this.client.settingsCache.get( g.id ) ) {
+          await this.client.db.insertSettings( g.id, g.name );
+          this.client.updateCache();
+        }
+      } );
+
+      setInterval( async() => {
+        await ActivityUpdate.fetch( this.client );
+      }, 20000 );
+
+      this.client.log.data(
+        `${this.client.user.tag}, ready to roll! ${this.client.users.size} users in ${this.client.guilds
+          .size} servers.`
+      );
+    } catch ( err ) {
+      this.client.log.error( `Error in ready event: ${err}` );
     }
-
-    setInterval( async() => {
-      await ActivityUpdate.fetch( this.client );
-    }, 20000 );
-
-    this.client.log.data(
-      `${this.client.user.tag}, ready to roll! ${this.client.users.size} users in ${this.client.guilds
-        .size} servers.`
-    );
   }
 };
