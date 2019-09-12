6e3ec1b3513dd2254dac2ac4f4c0f5d3e1c26d3b
Dan <headhertz88@gmail.com>
2019-09-12 21:43:24 +0100

    typo

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -5,10 +5,9 @@ module.exports = class About extends Command {
   constructor( ...args ) {
     super( ...args, {
       name: 'about',
-      description: '...',
+      description: 'About the bot',
       category: 'General',
-      usage: '.about',
-      extended: '...'
+      usage: '.about'
     } );
   }
 
@@ -20,7 +19,7 @@ module.exports = class About extends Command {
 
         The issue is that in channels dedicated to artist feedback many users do not give others feedback, only dropping their tracks and expecting other people to give them feedback. This causes these channels to be flooded with spam and provide little value. The bot prevents anyone from sharing their music unless they pass a requirement of providing feedback to others first. It does this by analysing the messages in the channel and using an algorithm to assign a score, once the threshold has been passed the bot will allow users to post their track, updating records in the process.
         
-        There are several other beneficial commands, and an experience/leveling system in order to gamify the process in the hopes to encourage and reward those who contribute the most.`
+        There are several other beneficial commands, and an experience/leveling system in order to gamify the process in the hope to encourage and reward those who contribute the most.`
       )
       .setColor( '00d919' )
       .addField( 'Author:', '<@113226391771717632>', true )
